36d0287e8c280e42af8a90ce1890c72fdbdfcff0
Anna Marie Cooper and Logan Tanous <logan@>
2018-02-06 11:48:23 -0800

    did everything else

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -3,8 +3,20 @@ require('scrabble_score')
 
 
 describe('#scrabble_score') do
-  it("returns a scrabble score for a letter") do
-  word = Word.new("a")
+  it("returns a scrabble score of 1 for letter 'a'") do
+  word = Word.new("A")
   expect(word.scrabble_score()).to(eq(1))
+  end
+  it("returns a scrabble score of 5 for word 'cat'") do
+  word = Word.new("CAT")
+  expect(word.scrabble_score()).to(eq(5))
+  end
+  it("returns a scrabble score of 5 for word lowercase 'cat'") do
+  word = Word.new("cat")
+  expect(word.scrabble_score()).to(eq(5))
+  end
+  it("returns a scrabble score of 16 for word 'zebra'") do
+  word = Word.new("zebra")
+  expect(word.scrabble_score()).to(eq(16))
   end
 end
